9c805dd2172506485eb3acd30007bac1440b54c5
Ugedya <d9d9fed9@gmail.com>
2026-01-07 23:58:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
 app.use(express.json());
 
 const token = process.env.BOT_TOKEN;
